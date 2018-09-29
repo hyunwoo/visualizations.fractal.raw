e4c28bfc44ddc460e76a204aa868d23acc9c6b16
h.hyunwoo@gmail.com <Angles12>
2018-09-29 18:24:48 +0900

    image 로딩 수정

--- fractal.generate.js
+++ fractal.generate.js
@@ -1,8 +1,10 @@
+var img;
+
 function setup() {
+  img = loadImage('images_01.jpg');
   var $root = $('#renderer');
   var canvas = createCanvas($root.width(), $root.height());
   canvas.parent('renderer');
-
 
   $('.command[type=generate]').on('click', () => {
     var inputs = $('input');
@@ -10,24 +12,30 @@ function setup() {
 
     for (var i = 0; i < inputs.length; i++) {
       var $input = $(inputs[i]);
-      var $value = $input.siblings('.texts .value');
-      $input.on('change', () => {
-        console.log('??');
-        $value.text($input.val());
-      });
       var val = $input.val();
       var key = $input.attr('name');
       dataSet[key] = val;
     }
 
+
+    // 자아아아알하면 갈수 있겠는데!!!!
+
+
+    // var a = { name : 'juwon'};
+    // a.name
+    // 강아지..?
+    // ????????????????
+    // a['name'] = 'hyunwoo';
+
     var fractalGenerator = new FractalGenerator(dataSet);
     fractalGenerator.generate();
-  })
+  });
 
-  $('canvas').attr('id','renderer');
-
-
+  // $() jquery Selector 에서 attr 함수는 key , value
+  // 즉, $().attr('id','renderer') 라고하면, 선택된 dom의 id가 renderer
+  // $('canvas').attr('id','renderer');
 }
+
 
 $('.command[type=download]').on('click', function () {
   var canvas = document.getElementsByTagName('canvas')[0];
@@ -36,7 +44,9 @@ $('.command[type=download]').on('click', function () {
     type: 'jpg',
     quality: 1
   });
-})
+});
+
+// value change setup
 
 var inputs = $('input');
 for (var i = 0; i < inputs.length; i++) {
@@ -65,11 +75,10 @@ function FractalGenerator(option) {
   // 선을 그릴 떄
   // 1. 점과 점 을 연결
   // 2. 시작점과 각도와 길이
-
+  console.log('generate');
 
   var startColor = '#ff0000';
   var endColor = '#0000ff';
-
 
   this.lerpColor = (c1, c2, lerpValue) => {
     var r1 = Number('0x' + c1[1] + c1[2]);
@@ -84,8 +93,7 @@ function FractalGenerator(option) {
       r: r1 * (1 - lerpValue) + r2 * lerpValue,
       g: g1 * (1 - lerpValue) + g2 * lerpValue,
       b: b1 * (1 - lerpValue) + b2 * lerpValue,
-    }
-
+    };
   };
 
   this.drawLine = (x1, y1, degree, dist, depth) => {
@@ -93,10 +101,10 @@ function FractalGenerator(option) {
     if (depth > option.depthCount) return;
 
     var radian = degree / 180 * Math.PI;
-
     var x2 = x1 + Math.cos(radian) * dist;
     var y2 = y1 + Math.sin(radian) * dist;
-    var c = that.lerpColor(option.startColor, option.endColor, depth / option.depthCount);
+    var c = that.lerpColor(option.startColor, option.endColor,
+      depth / option.depthCount);
 
     // stroke(c.r, c.g, c.b);
     var a = (1 - depth / option.depthCount) * 255;
@@ -105,7 +113,6 @@ function FractalGenerator(option) {
     line(x1, y1, x2, y2);
 
     // var childCount = 2; // temp
-
     var initAngle = degree - ((option.childCount - 1) * option.childAngle) / 2;
     for (var i = 0; i < option.childCount; i++) {
       that.drawLine(x2, y2,
@@ -114,10 +121,10 @@ function FractalGenerator(option) {
     }
   };
 
-
   this.generate = () => {
     blendMode(BLEND);
     background(0);
+    image(img, 0, 0, width * img.width / img.height, height);
     var createCount = option.branchCount * 1;
     var diffAngle = 360 / createCount;
 
@@ -125,14 +132,9 @@ function FractalGenerator(option) {
     for (var i = 0; i < createCount; i++) {
       that.drawLine(width / 2, height / 2, i * diffAngle, option.branchLength, 1);
     }
-
   };
-
   return this;
 }
 
-
 function draw() {
-
 }
-
